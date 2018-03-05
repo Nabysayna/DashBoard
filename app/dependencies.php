a130a9c642c504645277c16c8188e58b81dda5f6
okako <kasanesn@hotmail.com>
2018-03-05 11:49:46 +0000

    first commit

---
+++ app/dependencies.php
@@ -0,0 +1,19 @@
+<?php
+
+// DIC configuration
+
+$container = $app->getContainer();
+
+// --------------------------------------------------------------
+// Service providers
+// --------------------------------------------------------------
+
+$container['view'] = new \Slim\Views\PhpRenderer(DIR . '/app/templates/');
+
+// monolog
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler(DIR."/tmp/logs/".date('Y-m').".log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
