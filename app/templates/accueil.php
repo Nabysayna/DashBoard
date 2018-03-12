2bab4b59f4e44b35cce247a48974cf8e78927b80
marame <ndiayemarame3@gmail.com>
2018-03-12 17:20:22 +0000

    tigopartie

--- app/templates/accueil.php
+++ app/templates/accueil.php
@@ -183,25 +183,7 @@
 </div>
 </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-			<div class="modal-title titremod" id="" style="display:none;"></div>
+<div class="modal-title titremod" id="" style="display:none;"></div>
 			<button type="button" name="rechercherorange" class="" id="recher" background-color:orange; margin:auto;"  onclick="myRechercheorange()"><span style="color:black;">Rechercher</span></button>
             </div></div>
             <div class="col-sm-3">
@@ -338,21 +320,306 @@
                      <div class="col-sm-2 col-xs-2">
                      <div ><button type="button" id=rn1 class="" onclick="reinitialiser(this)" ><span style="color:black;">Reinitialiser</span></button></div></div></button></div></div>
 
-				<div class="row tigo" style="background-color:green">
-				<div class="col-md-8 col-xs-3">
-               <h1 style="color:black;"> TigoCash</h1></div>
-				<div class="col-md-4 col-xs-2">
-				<button type="button" class="" data-toggle="modal" data-target=".bd-example-modal-sm"><span id="span">Trafic</span></button>      
 
 
-				  <div class="col-sm-1" id="idphone" style="width:20px; color:black;">Id</div>
-					<div class="col-sm-3" style="color:black;">Etats</div>
+		<div  class="col-sm-6 col-md-6 col-xs-12" style="background-color:green">
+			<div class="row tigoprincipale" >
+				<div class="row tigo" style="background-color:" >
+				<div class="col-md-8 col-xs-1">
+              <div><h1 style="color:black;"> TigoCash</h1></div></div></div></div>
+			<div>
+		<div class="col-md-10 col-xs-1" align="right"><button type="button" class="" data-toggle="modal" data-target=".bd-example-modal-sm" ><span id="span">Trafic</span></button>
+					 </div>
+<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" style="background-color:green">
+				  <div class="modal-dialog modal-sm" style="background-color:green">
+					<div class="modal-content" id="modaltrafic" style="background-color:green">
+					 <div class="row" id="modalmodal">
+					  <div class="col-sm-2">
+					  <div id="idd">1</div></div>
+					  <div class="form-check">
+					  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+					  </div></div>
+					  <div class="row" id="modalmodal">
+					  <div class="col-sm-2">
+					  <div id="idd">2</div></div>
+				   	  <div class="form-check">
+					  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+					  </div> </div>
+                      <div class="row" id="modalmodal">
+					  <div class="col-sm-2">
+					  <div id="idd">3</div></div>
+					  <div class="form-check">
+					  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+					  </div> </div>
+                      <div class="row" id="modalmodal">
+					  <div class="col-sm-2">
+					  <div id="idd">4</div></div>
+					  <div class="form-check">
+					  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+					  </div> </div>
+					  <div class="row" id="modalmodal">
+					  <div class="col-sm-2">
+					  <div id="idd">5</div></div>
+					  <div class="form-check">
+					  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+					  </div> </div>
+					  <div class="row" id="modalmodal">
+					  <div class="col-sm-2">
+					  <div id="idd">6</div></div>
+					  <div class="form-check">
+					  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+					  </div> </div>
+					  <div class="form-check">
+					  </div>
+                      <div class="modal-footer">
+						<button type="button" class="" onclick="valider()">Valider</button>
+						<button type="button" class="" data-dismiss="modal">Close</button>
+					 </div>
+						
+
+
+					</div></div></div>
+
+					<div class="row orange" >
+                   <div class="col-sm-1" id="idphone" style="width:20px; color:black;">Id</div>
+				  <div class="col-sm-3" style="color:black;">Etats</div>
 				  <div class="col-sm-8" style="color:black;">Les opérations</div>
+				  </div>
+				  <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+				  <div class="modal-dialog modal-sm" style="background-color:black">
+					<div class="modal-content" id="modaltrafic" style="background-color:">
+
+</div></div></div>
+<div class="row orange" >
+			<div class="col-sm-2">
+				<div id="idd">1</div>
+			</div>
+			<div class="col-sm-1">
+				<p class="stateChecker" style="width: 3rem ; height: 3rem ;"></p>
+			</div>
+			<div class="col-sm-3 col-xs-2">
+				<div>
+					<button type="button" id=test1 onclick="myFunction(1)" class="" data-toggle="modal" data-target="#exampleModal">
+						<span style="color:black;">Rechercher</span>
+					</button>
+					
+				</div>
+			</div>
+			<div class="col-sm-3 col-xs-2"><button type="button" id="remonterorange" class="" data-toggle="modal" data-target=".bd-example-modal-lg"  >
+				<span style="color:black;">Remonter</span></button>
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+				  <div class="modal-dialog modal-lg">
+					<div class="modal-content" style="margin:20px;">
+						<form style="margin:20px;" method="POST" action="lireexcel.php" class="form" enctype="multipart/form-data">
+						  <div class="form-group">
+							<label for="fichier"><i class="fa fa-upload" aria-hidden="true" style=""></i>Importer un fichier Excel</label>
+								<!--<input type="file" class="form-control-file" id="exampleFormControlFile1" accept=".doc,docx,x" onchange="myFunction11()"/>-->
+								<input id="fichierorange" name="fichier" class="form-control-file"  type="file" accept=".xls" ></br>
+								<input type="submit"  value="remonter" name="submit">
+							</div>
+						</form>
 					</div>
-</div></div></div></div>
+
+</div></div></div>
+<div class="col-sm-2 col-xs-2">
+<div ><button type="button" id=rn1 class="" onclick="reinitialiser(this)" ><span style="color:black;">Reinitialiser</span></button></div>
+</div></div>
+
+<div class="row orange" >
+			<div class="col-sm-2">
+				<div id="idd">2</div>
+			</div>
+			<div class="col-sm-1">
+				<p class="stateChecker" style="width: 3rem ; height: 3rem ;"></p>
+			</div>
+			<div class="col-sm-3 col-xs-2">
+				<div>
+					<button type="button" id=test2 onclick="myFunction(2)" class="" data-toggle="modal" data-target="#exampleModal">
+						<span style="color:black;">Rechercher</span>
+					</button>
+					
+				</div>
+			</div>
+
+<div class="col-sm-3 col-xs-2">
+<button type="button" id="remonterorange" class="" data-toggle="modal" data-target=".bd-example-modal-lg"  >
+				<span style="color:black;">Remonter</span></button>
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+				  <div class="modal-dialog modal-lg">
+					<div class="modal-content" style="margin:20px;">
+						<form style="margin:20px;" method="POST" action="lireexcel.php" class="form" enctype="multipart/form-data">
+						  <div class="form-group">
+							<label for="fichier"><i class="fa fa-upload" aria-hidden="true" style=""></i>Importer un fichier Excel</label>
+								<!--<input type="file" class="form-control-file" id="exampleFormControlFile1" accept=".doc,docx,x" onchange="myFunction11()"/>-->
+								<input id="fichierorange" name="fichier" class="form-control-file"  type="file" accept=".xls" ></br>
+								<input type="submit"  value="remonter" name="submit">
+							</div>
+						</form>
+					</div>
+
+				</div></div></div>
+<div class="col-sm-2 col-xs-2">
+<div ><button type="button" id=rn1 class="" onclick="reinitialiser(this)" ><span style="color:black;">Reinitialiser</span></button></div>
+</div></div>
+
+            
+<div class="row orange" >
+		<div class="col-sm-2">
+		<div id="idd">3</div></div>
+		<div class="col-sm-1">
+			<p class="stateChecker" style="width: 3rem ; height: 3rem ;"></p>
+			</div>			
+		    <div class="col-sm-3 col-xs-2">
+				<div>
+		<button type="button" id="test1" onclick="myFunction(1)" class="" data-toggle="modal" data-target="#exampleModal"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Rechercher</font></font></button>
+		</div></div>
 
 
-           
+<div class="col-sm-3 col-xs-2">
+<button type="button" id="remonterorange" class="" data-toggle="modal" data-target=".bd-example-modal-lg"  >
+				<span style="color:black;">Remonter</span></button>
+				<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+				  <div class="modal-dialog modal-lg">
+					<div class="modal-content" style="margin:20px;">
+						<form style="margin:20px;" method="POST" action="lireexcel.php" class="form" enctype="multipart/form-data">
+						  <div class="form-group">
+							<label for="fichier"><i class="fa fa-upload" aria-hidden="true" style=""></i>Importer un fichier Excel</label>
+								<!--<input type="file" class="form-control-file" id="exampleFormControlFile1" accept=".doc,docx,x" onchange="myFunction11()"/>-->
+								<input id="fichierorange" name="fichier" class="form-control-file"  type="file" accept=".xls" ></br>
+								<input type="submit"  value="remonter" name="submit">
+							</div>
+						</form>
+					</div></div></div></div>
+
+<div class="col-sm-2 col-xs-2">
+<div ><button type="button" id=rn1 class="" onclick="reinitialiser(this)" ><span style="color:black;">Reinitialiser</span></button></div>
+</div>
+					</div>
+
+
+				
+<div class="row orange" >
+			<div class="col-sm-2">
+				<div id="idd">4</div>
+			</div>
+			<div class="col-sm-1">
+				<p class="stateChecker" style="width: 3rem ; height: 3rem ;"></p>
+			</div>
+			<div class="col-sm-3 col-xs-2">
+				<div>
+					<button type="button" id=test4 onclick="myFunction(4)" class="" data-toggle="modal" data-target="#exampleModal">
+						<span style="color:black;">Rechercher</span>
+					</button>
+					</div>
+				</div>
+
+
+
+				<div class="col-sm-3 col-xs-2">
+<button type="button" id="remonterorange" class="" data-toggle="modal" data-target=".bd-example-modal-lg"  >
+				<span style="color:black;">Remonter</span></button>
+
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+				  <div class="modal-dialog modal-lg">
+					<div class="modal-content" style="margin:20px;">
+						<form style="margin:20px;" method="POST" action="lireexcel.php" class="form" enctype="multipart/form-data">
+						  <div class="form-group">
+							<label for="fichier"><i class="fa fa-upload" aria-hidden="true" style=""></i>Importer un fichier Excel</label>
+								<!--<input type="file" class="form-control-file" id="exampleFormControlFile1" accept=".doc,docx,x" onchange="myFunction11()"/>-->
+								<input id="fichierorange" name="fichier" class="form-control-file"  type="file" accept=".xls" ></br>
+								<input type="submit"  value="remonter" name="submit">
+							</div>
+						</form>
+					</div></div></div></div>
+
+<div class="col-sm-2 col-xs-2">
+<div ><button type="button" id=rn1 class="" onclick="reinitialiser(this)" ><span style="color:black;">Reinitialiser</span></button></div>
+</div>
+					</div>
+
+
+
+<div class="row orange" >
+			<div class="col-sm-2">
+				<div id="idd">5</div>
+			</div>
+			<div class="col-sm-1">
+				<p class="stateChecker" style="width: 3rem ; height: 3rem ;"></p>
+			</div>
+			<div class="col-sm-3 col-xs-2">
+				<div>
+					<button type="button" id=test5 onclick="myFunction(5)" class="b" data-toggle="modal" data-target="#exampleModal">
+						<span style="color:black;">Rechercher</span>
+					</button>
+					
+				</div></div>
+
+				<div class="col-sm-3 col-xs-2">
+<button type="button" id="remonterorange" class="" data-toggle="modal" data-target=".bd-example-modal-lg"  >
+				<span style="color:black;">Remonter</span></button>
+
+
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+				  <div class="modal-dialog modal-lg">
+					<div class="modal-content" style="margin:20px;">
+						<form style="margin:20px;" method="POST" action="lireexcel.php" class="form" enctype="multipart/form-data">
+						  <div class="form-group">
+							<label for="fichier"><i class="fa fa-upload" aria-hidden="true" style=""></i>Importer un fichier Excel</label>
+								<!--<input type="file" class="form-control-file" id="exampleFormControlFile1" accept=".doc,docx,x" onchange="myFunction11()"/>-->
+								<input id="fichierorange" name="fichier" class="form-control-file"  type="file" accept=".xls" ></br>
+								<input type="submit"  value="remonter" name="submit">
+							</div>
+						</form>
+					</div>
+				</div>
+			</div></div>
+<div class="col-sm-2 col-xs-2">
+<div ><button type="button" id=rn1 class="" onclick="reinitialiser(this)" ><span style="color:black;">Reinitialiser</span></button></div>
+</div>
+			</div>
+
+
+
+
+
+
+<div class="row orange" >
+			<div class="col-sm-2">
+				<div id="idd">6</div>
+			</div>
+			<div class="col-sm-1">
+				<p class="stateChecker" style="width: 3rem ; height: 3rem ;"></p>
+			</div>
+			<div class="col-sm-3 col-xs-2">
+				<div>
+					<button type="button" id=test6 onclick="myFunction(6)" class="" data-toggle="modal" data-target="#exampleModal">
+						<span style="color:black;">Rechercher</span>
+					</button>
+					
+				</div>
+			</div>
+							<div class="col-sm-3 col-xs-2">
+<button type="button" id="remonterorange" class="" data-toggle="modal" data-target=".bd-example-modal-lg"  >
+				<span style="color:black;">Remonter</span></button>
+
+
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+				  <div class="modal-dialog modal-lg">
+					<div class="modal-content" style="margin:20px;">
+						<form style="margin:20px;" method="POST" action="lireexcel.php" class="form" enctype="multipart/form-data">
+						  <div class="form-group">
+							<label for="fichier"><i class="fa fa-upload" aria-hidden="true" style=""></i>Importer un fichier Excel</label>
+								<!--<input type="file" class="form-control-file" id="exampleFormControlFile1" accept=".doc,docx,x" onchange="myFunction11()"/>-->
+								<input id="fichierorange" name="fichier" class="form-control-file"  type="file" accept=".xls" ></br>
+								<input type="submit"  value="remonter" name="submit">
+							</div>
+						</form>
+						</div></div></div></div>
+<div class="col-sm-2 col-xs-2">
+<div ><button type="button" id=rn1 class="" onclick="reinitialiser(this)" ><span style="color:black;">Reinitialiser</span></button></div>
+</div>
+						</div>
+
 </body>
 
 <script src="http://code.jquery.com/jquery.js"></script>
